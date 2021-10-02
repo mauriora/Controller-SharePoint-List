1d8928155688707b2a408eb762027e3ed6e53c48
denniskuhn <dennis@kuhn.geek.nz>
2021-10-02 19:21:31 +1300

    Select fields and returnerror  type

--- src/controller/Graph/User.ts
+++ src/controller/Graph/User.ts
@@ -3,10 +3,56 @@ import "@pnp/graph/users";
 import { User } from '@microsoft/microsoft-graph-types';
 export { User } from '@microsoft/microsoft-graph-types';
 
-export const getUser = async (emailOrId: string): Promise<User | void> => {
-    const matchingUser = await graph.users.getById(emailOrId)();
+const USER_FIELDS = ['businessPhones', 'displayName', 'jobTitle', 'mobilePhone'];
 
-    console.log(`Graph/User:getUser( ${emailOrId} ) ${matchingUser?.displayName}`, matchingUser );
+const cache = new Map<string, User>();
 
-    return matchingUser;
+interface Error403 {
+    isHttpRequestError: true;
+    status: 403;
+    statusText: string;
+    message: string;
+}
+
+const is403Error = (err: unknown | Error403): err is Error403 =>
+    err && typeof err === 'object' &&
+    (err as Error403).isHttpRequestError === true &&
+    (err as Error403).status === 403 &&
+    typeof (err as Error403).statusText === 'string' &&
+    typeof (err as Error403).message === 'string';
+
+export class ErrorWithInner<InnerType extends unknown = unknown> extends Error {
+    constructor(message?: string, public inner?: InnerType) {
+        super(message);
+    }
+}
+
+export const getUser = async (emailOrId: string, selects?: string[]): Promise<User | void> => {
+    const existing = cache.get(emailOrId);
+    if (existing) {
+        return existing;
+    }
+    try {
+        const matchingUser = await graph.users.getById(emailOrId).select(...(selects ?? USER_FIELDS))();
+
+        if (matchingUser) {
+            cache.set(emailOrId, matchingUser);
+        }
+        return matchingUser;
+    } catch (err) {
+        let newError: ErrorWithInner | ErrorWithInner<Error403>;
+        if (is403Error(err)) {
+            newError = new ErrorWithInner(
+                `Graph/getUser(${emailOrId}): please ensure the permissions 'Microsoft Graph User.Read.All'\n` +
+                ' are requested in: app/YOUR-Extension/config/package-solution.json:' +
+                ' solution.webApiPermissionRequests: { "resource": "Microsoft Graph", "scope": "User.Read"}\n' +
+                ' and approved at:' +
+                ' \'https://YOURORGANISATION-admin.sharepoint.com/_layouts/15/online/AdminHome.aspx#/webApiPermissionManagement\'',
+                err);
+            newError.name = `[${err.status}] ${err.statusText}`;
+        } else {
+            newError = new ErrorWithInner(`Graph/getUser(${emailOrId}) caught ${(err as Error)?.message}`, err);
+        }
+        throw newError;
+    }
 }
